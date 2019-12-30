c423fa2eb8a3261df57e2a69898264a8edc85b5a
Steve Mullins <sgmullins@gmail.com>
2019-12-30 08:10:14 -0800

    update user with image, connect to db

--- models/user.js
+++ models/user.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose')
+
 const UserSchema = new Schema({
 	email: String,
+	image: String,
 	workspots: [{
 		type: Schema.Types.ObjectId,
 		ref: 'Workspot'
